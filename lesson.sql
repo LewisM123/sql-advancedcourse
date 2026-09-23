8d04c478616d92ace996749c0a6f175fc836ac49
LewisM123 <Lewis.McShane@lscft.nhs.uk>
2026-09-23 12:33:53 +0000

    completed select basics

--- lesson.sql
+++ lesson.sql
@@ -1,8 +1,25 @@
 /*
 Example lesson stub
 */
+SELECT   ps.PatientId,
+         ps.AdmittedDate,
+         ps.DischargeDate,
+         ps.Hospital,
+         ps.Ward,
+         ps.ethnicity,
+         DATEDIFF(day, ps.AdmittedDate, ps.DischargeDate) AS LengthOfStay
+FROM     PatientStay AS ps
+WHERE    ps.hospital IN ('Kingston', 'PRUH')
+         AND --AND ps.Ward LIKE '%Surgery'
+         ps.AdmittedDate BETWEEN DATEFROMPARTS(2024, 2, 28) AND DATEFROMPARTS(2024, 3, 1)
+ORDER BY LengthOfStay DESC, ps.AdmittedDate DESC;
 
-SELECT
-	*
-FROM
-	PatientStay ps ;
+SELECT   ps.hospital,
+         ps.ward,
+         SUM(ps.tariff) AS TotalTariff,
+         MAX(ps.Tariff) AS biggestTariff,
+         COUNT(*) AS TotalPatients
+FROM     PatientStay AS ps
+GROUP BY ps.Hospital, ps.ward
+HAVING   SUM(ps.tariff) >= 10
+ORDER BY TotalTariff DESC;
